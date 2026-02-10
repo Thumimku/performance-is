a84aed66bdb2f30e64a29bdf532036a030115bc4
thumimku <thumilan.15@cse.mrt.ac.lk>
2026-02-10 10:13:56 +0530

    50 -> 500

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -264,7 +264,7 @@ elif [ "$concurrency" == "1000-3000" ]; then
     default_concurrent_users="1000 1500 2000 2500 3000"
 elif [ "$concurrency" == "50-50" ]; then
     echo "Running tests for concurrency level 500"
-    default_concurrent_users="50"
+    default_concurrent_users="500"
 elif [ "$concurrency" == "50-1000" ]; then
     echo "Running tests for concurrency level 50-1000"
     default_concurrent_users="50 100 150 300 500 750 1000"
