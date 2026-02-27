226950f8d3ebf3d53d2fc893e1865fe094299e6d
thumimku <thumilan.15@cse.mrt.ac.lk>
2026-02-27 20:43:31 +0530

    code without consent

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -89,15 +89,15 @@ declare -A test_scenario04=(
 #    [skip]=false
 #    [modes]="FULL QUICK"
 # )
-# declare -A test_scenario08=(
-#    [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
-#    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
-#    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-#    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-#    [tenantMode]=false
-#    [skip]=false
-#    [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
-# )
+declare -A test_scenario08=(
+   [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
+   [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
+   [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+   [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
+   [tenantMode]=false
+   [skip]=false
+   [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
+)
 # declare -A test_scenario09=(
 #     [name]="09-oidc_password_grant"
 #     [display_name]="OIDC Password Grant Type"
