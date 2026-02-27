af342f407f05c600680998e1eee6fc3192c7fdcb
thumimku <thumilan.15@cse.mrt.ac.lk>
2026-02-27 14:39:14 +0530

    code

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -53,15 +53,15 @@
 #    [skip]=false
 #    [modes]="FULL QUICK"
 # )
-# declare -A test_scenario04=(
-#    [name]="04-oidc_auth_code_redirect_with_consent_retrieve_user_attributes_groups_and_roles"
-#    [display_name]="OIDC Auth Code Grant Redirect With Consent Retrieve User Attributes Groups and Roles"
-#    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-#    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-#    [tenantMode]=false
-#    [skip]=false
-#    [modes]="FULL QUICK"
-# )
+declare -A test_scenario04=(
+   [name]="04-oidc_auth_code_redirect_with_consent_retrieve_user_attributes_groups_and_roles"
+   [display_name]="OIDC Auth Code Grant Redirect With Consent Retrieve User Attributes Groups and Roles"
+   [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+   [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
+   [tenantMode]=false
+   [skip]=false
+   [modes]="FULL QUICK"
+)
 # declare -A test_scenario05=(
 #    [name]="05-oidc_auth_code_redirect_without_consent"
 #    [display_name]="OIDC Auth Code Grant Redirect Without Consent"
@@ -125,15 +125,15 @@
 #     [skip]=false
 #     [modes]="FULL QUICK"
 # )
-declare -A test_scenario12=(
-    [name]="12-oidc_password_grant_retrieve_user_attributes_groups_and_roles"
-    [display_name]="OIDC Password Grant Type Retrieve User Attributes Groups and Roles"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-    [jmx]="oidc/OIDC_Password_Grant_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
-)
+# declare -A test_scenario12=(
+#     [name]="12-oidc_password_grant_retrieve_user_attributes_groups_and_roles"
+#     [display_name]="OIDC Password Grant Type Retrieve User Attributes Groups and Roles"
+#     [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
+#     [jmx]="oidc/OIDC_Password_Grant_Retrieve_User_Attributes_Groups_And_Roles.jmx"
+#     [tenantMode]=false
+#     [skip]=false
+#     [modes]="FULL QUICK"
+# )
 # declare -A test_scenario13=(
 #    [name]="13-saml2_sso_redirect_binding"
 #    [display_name]="SAML2 SSO Redirect Binding"
